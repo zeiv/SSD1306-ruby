091d7d8a4d11e4b8dfb18ab806d11fe8eb329aa1
Jacob Killelea <Jkillelea@protonmail.ch>
2017-01-05 14:39:20 -0800

    update

--- lib/SSD1306/display.rb
+++ lib/SSD1306/display.rb
@@ -199,20 +199,17 @@ module SSD1306
       raise 'Contrast not yet implemented'
     end
 
-    # TODO: Implement Dimming functionality
     # Dim the display
-    # dim = true: display is dimmed
-    # dim = false: display is normal
     def dim(dim)
-      # raise 'Dim not implemented yet'
-      if dim
-        contrast = 0
-      elsif @vccstate == SSD1306_EXTERNALVCC
-        contrast = 0x9F
-      else
-        contrast = 0xCF
-      end
-      puts contrast
+      # dim = true: display is dimmed
+      # dim = false: display is normal
+      contrast = 0 # assume dim
+      if not dim
+        if @vccstate == SSD1306_EXTERNALVCC
+          contrast = 0x9F
+        else
+          contrast = 0xCF
+        end
       self.command SSD1306_SETCONTRAST
       self.command contrast
     end
